37e4b2b3ceebd44a1babe0f7860caaa91c003676
Kyosuke INOUE <kyoro@www11441uf.sakura.ne.jp>
2012-11-18 17:44:46 +0900

    first commit

---
+++ webfront/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyChime::Application.config.secret_token = 'd6d840332382f6508024f4b26dda5ba5569e0d7dcf85b0e360ace34a8176d4519a47e07083d48613841e7b2c5c247317cea6556ad311d571255830f6a9fa86ed'
